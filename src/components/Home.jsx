2fc762d23a199aafa6c38faf620ded3b8ce362e5
SZC1702 <z.carvalho.sergio@gmail.com>
2025-04-29 08:22:11 -0300

    feat: start form page (#1)

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <p>Esta é a página inicial do sistema.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/FormPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FormPage = () => {
+  return (
+    <div>
+      <h1>Form Page</h1>
+      <p>Esta é uma página de formulário básica.</p>
+    </div>
+  );
+};
+
+export default FormPage;
