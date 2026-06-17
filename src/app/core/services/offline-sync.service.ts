74d2698ba24e98a4db96cd743e6185b0fb48ab87
ElizioMartins <seu-email@github.com>
2026-06-17 15:40:01 -0300

    Adiciona mock e plano

---
+++ src/app/core/services/offline-sync.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { MOCK_ALOCACOES } from '../../../environments/offline-mock.data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfflineSyncService {
+
+  constructor() { }
+
+  // Simula o GET da API (com 1 segundo de atraso para parecer real)
+  getAlocacoesMock(): Observable<any[]> {
+    return of(MOCK_ALOCACOES).pipe(delay(1000));
+  }
+
+  // Simula o POST de Sincronização
+  syncOcorrenciasMock(filaOffline: any[]): Observable<any> {
+    console.log('🚀 Enviando para a API (Mock):', filaOffline);
+    // Simula um retorno de sucesso da API
+    return of({ success: true, message: 'Sincronizado com sucesso!' }).pipe(delay(1500));
+  }
+}
---
+++ src/environments/offline-mock.data.ts
@@ -0,0 +1,42 @@
+export const MOCK_ALOCACOES = [
+  {
+    celebrationJobLocationId: "2972b9c0-56fe-44d8-8277-5c1701c27dcc", // Base 1 Caveira
+    localNome: "Base 1 Caveira",
+    memberId: "736c600c-3d4b-4afd-bbb7-45826f724acf",
+    membroNomeColete: "45",
+    membroNome: "Elizio Martins Cezarino",
+    statusAtualEnum: "scheduler" // Agendado
+  },
+  {
+    celebrationJobLocationId: "2972b9c0-56fe-44d8-8277-5c1701c27dcc",
+    localNome: "Base 1 Caveira",
+    memberId: "123c600c-3d4b-4afd-bbb7-45826f724123",
+    membroNomeColete: "12",
+    membroNome: "João Silva",
+    statusAtualEnum: "start" // Em Turno
+  },
+  {
+    celebrationJobLocationId: "2972b9c0-56fe-44d8-8277-5c1701c27dcc",
+    localNome: "Base 1 Caveira",
+    memberId: "999c600c-3d4b-4afd-bbb7-45826f724999",
+    membroNomeColete: "77",
+    membroNome: "Carlos Alberto",
+    statusAtualEnum: "scheduler"
+  },
+  {
+    celebrationJobLocationId: "d747b2ca-409a-498a-ae85-a3dfd373b5d5", // Saída Parque
+    localNome: "Saida Parque",
+    memberId: "444c600c-3d4b-4afd-bbb7-45826f724444",
+    membroNomeColete: "88",
+    membroNome: "Maria Souza",
+    statusAtualEnum: "scheduler"
+  },
+  {
+    celebrationJobLocationId: "d747b2ca-409a-498a-ae85-a3dfd373b5d5",
+    localNome: "Saida Parque",
+    memberId: "555c600c-3d4b-4afd-bbb7-45826f724555",
+    membroNomeColete: "09",
+    membroNome: "Pedro Santos",
+    statusAtualEnum: "end_success" // Encerrado
+  }
+];
